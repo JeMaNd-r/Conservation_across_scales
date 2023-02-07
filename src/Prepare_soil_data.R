49ee34088b668e32945c12ff163c19aad03a04ae
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2023-02-07 17:31:56 +0100

    Add elevation data

--- src/Prepare_soil_data.R
+++ src/Prepare_soil_data.R
@@ -14,25 +14,25 @@ library(tidyverse)
 data_glob <- read_csv(paste0(here::here(), "/data_raw/GlobalAtlasv2_conservation_heterogeneity_papers_v1.csv"))
 data_glob
 
-data_cont <- read_csv()
+#data_cont <- read_csv()
 
-data_regi <- read_csv()
+#data_regi <- read_csv()
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Add missing environmental covariates ####
 
 # Elevation
-elev <- terra::rast(paste0(here::here(), "/data_raw/Elev_noGrid_WGS84.tif"))
-data_glob$Elevation <- terra::extract(elev, data_glob[,c("Longitude_c", "Latitude_c")])
+elev <- terra::rast(paste0(here::here(), "/data_raw/Elevation_Worldclim_Global.tif"))
+data_glob$Elevation <- terra::extract(elev, data_glob[,c("Longitude_c", "Latitude_c")])[,2]
 
 # Annual temperature
 annual_temp <- terra::rast(paste0(here::here(), "/data_raw/CHELSA_bio1_1981-2010_V.2.1.tif"))
-data_glob$AnnualTemp	 <- terra::extract(annual_temp, data_glob[,c("Longitude_c", "Latitude_c")])
-data_glob$AnnualTemp	 <- data_glob$AnnualTemp / 10
+data_glob$AnnualTemp <- terra::extract(annual_temp, data_glob[,c("Longitude_c", "Latitude_c")])[,2]
+data_glob$AnnualTemp <- data_glob$AnnualTemp / 10
 
 # Annual precipitation
 annual_prec <- terra::rast(paste0(here::here(), "/data_raw/CHELSA_bio12_1981-2010_V.2.1.tif"))
-data_glob$AnnualPrec <- terra::extract(annual_prec, data_glob[,c("Longitude_c", "Latitude_c")])
+data_glob$AnnualPrec <- terra::extract(annual_prec, data_glob[,c("Longitude_c", "Latitude_c")])[,2]
 
 head(data_glob)
 
