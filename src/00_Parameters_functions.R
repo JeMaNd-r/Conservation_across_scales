b5184503cdf032c30a660d323b7f84392a25be5f
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2023-02-23 17:51:25 +0100

    Add labels for functions

--- src/00_Parameters_functions.R
+++ src/00_Parameters_functions.R
@@ -6,17 +6,20 @@
 #                                           #
 #- - - - - - - - - - - - - - - - - - - - - -#
 
-# save sample function (if only 1 value in sample(), it considers it as vector)
+## re-define sample function (if only 1 value in sample(), it considers it as vector)
 resample <- function(x, ...) x[sample.int(length(x), ...)]
 
 
-# define functions to be compared
-fns = c("Soil_carbon_service", "OM_decomposition_service", "Water_regulation_service", 
+## define functions to be compared
+fns <- c("Soil_carbon_service", "OM_decomposition_service", "Water_regulation_service", 
         "Soil_stability_service", "Nutrient_service", "Pathogen_control", 
         "Richness_bacteria", "Fungi_18s_richness", "Invertebrate_18s_richness", 
         "Protist_18s_richness", "Ectomycorrhizal_18s_richness", 
         "Arbuscular_mycorrhizal_18s_richness", "Decomposers_18s_richness", 
         "Diss_Bacteria_std", "Diss_Fungi_std", "Diss_Protists_std", "Diss_invert_std")
+
+fns_labels <- read.csv(paste0(here::here(), "/data_raw/LABELS_functions.csv"),
+                       fileEncoding = "UTF-8-BOM")
 
 ## define variables to be compared between PA and nonPA, & threshold
 mahal_vars <- c("Latitude_c", "Longitude_c", "Elevation", "AnnualPrec", "AnnualTemp", 
