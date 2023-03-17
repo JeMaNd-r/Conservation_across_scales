066eeca63c651e380731176cceabf1a860ead4c6
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2023-03-17 09:10:40 +0100

    Fix add PA information (add type) and colinearity
    PA is now excluding not reported etc., and PA type column is added. Colinearity from Earthworm paper (Zeiss et al. (in review)).

--- src/01b_Prepare_soil_data.R
+++ src/01b_Prepare_soil_data.R
@@ -8,6 +8,7 @@
 
 library(here)
 library(tidyverse)
+library(usdm) # for colinearity check (VIF)
 
 source("src/00_Parameters_functions.R")
 
@@ -48,11 +49,48 @@ head(data_glob)
 # based on World Database on Protected Areas (WDPA) (protectedplanet.net)
 protect_glob <- read_csv(paste0(here::here(), "/intermediates/PA_assignment_global.csv"))
 
+# remove double entries (i.e., 2+ protected area types for 1 PA)
+protect_type <- data.frame("PA_type" = c("Ia", "Ib", "II", "III", "IV", "V", "VI",
+                                         "Not Reported", "Not Assigned", "Not Applicable"),
+                           "PA_rank" = 1:10,
+                           "PA_protected" = c(rep(1,7), rep(0,3)))
+protect_type
+
+protect_glob <- protect_glob %>% full_join(protect_type, by=c("IUCN_CAT" = "PA_type"))
+
+# take the highest = min (or lowest = max) protection level
+protect_glob <- protect_glob %>% 
+  group_by(id.y) %>% 
+  dplyr::select(id.y, PA_rank) %>%
+  summarize(across(everything(), list("min"=min, "max"=max), na.rm=T)) %>%
+  mutate("PA_rank_min" = ifelse(PA_rank_min == Inf, NA, PA_rank_min),
+         "PA_rank_max" = ifelse(PA_rank_max == -Inf, NA, PA_rank_max)) %>%
+  full_join(protect_type %>% rename("PA_type_min" = PA_type,
+                                    "PA_rank_min" = PA_rank,
+                                    "PA_protected_min" = PA_protected), 
+            by="PA_rank_min") %>%
+  full_join(protect_type %>% rename("PA_type_max" = PA_type,
+                                    "PA_rank_max" = PA_rank,
+                                    "PA_protected_max" = PA_protected), 
+            by="PA_rank_max")
+
+# We decided to take the minimum (i.e., highest) level of protection because
+# of sites such as ID 49 (Order_ID 302) that has min=1 (Ia) and max=8 (Not Reported)
+
 # add column with information about protected and non-protected sites
 data_glob <- data_glob %>% 
-  mutate("PA"=protect_glob %>% dplyr::select(id.y, PA) %>% unique() %>% dplyr::select(PA) %>% unlist())
-rm(protect_glob)
+  mutate("id.x" = 1:nrow(data_glob)) %>%
+  full_join(protect_glob %>% dplyr::select(id.y, PA_type_min, PA_protected_min),
+            by=c("id.x"="id.y")) %>%
+  rename("PA" = PA_protected_min,
+         "PA_type" = PA_type_min) %>%
+  mutate("PA" = ifelse(is.na(PA), 0, PA))
 
+nrow(data_glob %>% filter(PA==1)) #81
+nrow(data_glob %>% filter(!is.na(PA_type))) #136
+nrow(data_glob %>% filter(PA==0)) #305
+
+rm(protect_glob)
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Keep complete cases only ####
@@ -73,8 +111,47 @@ write_csv(data_glob, file=paste0(here::here(), "/intermediates/Data_global.csv")
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Check for colinearity between environmental variables ####
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+## Calculate variable inflation factor (VIF)
+# VIF is the extent of correlation between one predictor and all others.
+# The lower VIF, the better we can tell what predictor contributed (most) to the model
+env_glob <- as.data.frame(data_glob %>% 
+                dplyr::select(Latitude_c, Longitude_c, Soil_pH, 
+                              Soil_salinity, Clay_silt_c, Elevation,
+                              AnnualTemp, AnnualPrec))
 
+## VIF basen on raw data (explanatory raster stack)
+env_vif <- usdm::vif(env_glob)
+
+# which predictors should be excluded?
+vif_cor <- usdm::vifcor(env_glob, th=0.8)  #th = threshold correlation for exclusion
+# how: first find a pair of variables which has the maximum linear correlation 
+# (greater than th), and exclude one of them which has greater VIF. The 
+# procedure is repeated until no variable with a high correlation coefficient 
+# (grater than threshold) with other variables remains.
+
+vif_step <- usdm::vifstep(env_glob, th=10) #VIF >10
+
+# merge both data.frames
+env_vif <- env_vif %>% rename("VIF_raw" = VIF) %>% full_join(vif_step@results) %>%
+  full_join(as.data.frame(vif_cor@corMatrix) %>% mutate("Variables"=rownames(vif_cor@corMatrix)))
+
+env_vif
+
+write.csv(env_vif, file=paste0(here::here(), "/results/VIF_envVars_global.csv"), row.names = F)
+
+#- - - - - - - - - - - - - - - - - - - - - -
+## Calculate correlations between predictors
+# https://github.com/joaofgoncalves/GoncalvesAna_et_al_2021/tree/master/RCODE/PostModelAnalyses
+
+corMatSpearman <- cor(env_glob, use="complete.obs", method="spearman") %>% round(2)
+corMatPearson <- cor(env_glob, use="complete.obs", method="pearson") %>% round(2)
+
+write.csv(corMatSpearman, paste0("./results/corMatSpearman_envVars_global.csv"), row.names = F)
+write.csv(corMatPearson, paste0("./results/corMatPearson_envVars_global.csv"), row.names = F)
+
+rm(env_vif, vif_cor, corMatPearson, corMatSpearman, env_glob, vif_step)
 
 
 
