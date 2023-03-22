c8372e7f4189eec2d56d753473a5fd8df445fedc
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2023-03-22 15:50:08 +0100

    Remove t.test (p-values)
    p-values depend on sample size and do not add statistical information if we have the distribution of effect sizes.

--- src/03a_Compare_PA_nonPA.R
+++ src/03a_Compare_PA_nonPA.R
@@ -33,7 +33,6 @@ head(pa_pairs)
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Compare difference between PA and nonPA sites ####
 # take one run (1:1000), estimate p and effect size (and Bayesian), take next
-p_list <- vector("list", length = 1000)
 d_list <- vector("list", length = 1000)
 
 # progress bar
@@ -50,7 +49,6 @@ for(x in unique(pa_pairs$times)){
   temp_pairs <- pa_pairs[pa_pairs$times==x,]
   
   # Perform tests
-  p_list[[x]] <- list()
   d_list[[x]] <- list()
   
   # individual tests per LC type
@@ -74,44 +72,22 @@ for(x in unique(pa_pairs$times)){
                                     run=x,
                                     row.names = NULL)
     
-    for(no_fns in 1:(length(fns))){
-      # unpaired t test
-      temp_p <- t.test(temp_PA[,fns[no_fns]], 
-                       temp_nonPA[,fns[no_fns]])[c("p.value", 
-                                                   "conf.int", # 95% confidence intervals
-                                                   #"estimate", # mean of each group (1. PA, 2. nonPA)
-                                                   "statistic")] # t statistic
-      
-      p_list[[x]][[lc]][[no_fns]] <- data.frame(lc=lc, 
-                                           fns = fns[no_fns],
-                                           p_value = temp_p$p.value,
-                                           ci_lower = temp_p$conf.int[1],
-                                           ci_upper = temp_p$conf.int[2],
-                                           t_stats = temp_p$statistic,
-                                           run=x,
-                                           row.names = NULL) 
-    }
     #print(sum(temp_pairs[temp_pairs$LC==lc,"nonPA"] == temp_nonPA[,"Order_ID"])==min_size) # make sure that the sites are pairing properly
     # print should give min_size (TRUE = fitting pairs) for each LC types * 1000 runs
-    
-    p_list[[x]][[lc]] <- do.call(rbind, p_list[[x]][[lc]])
     
   }
   
   # combine individual list elements (df) into one df
   d_list[[x]] <- do.call(rbind, d_list[[x]])
-  p_list[[x]] <- do.call(rbind, p_list[[x]])
   
   # remove rownames
   rownames(d_list[[x]]) <- NULL
-  rownames(p_list[[x]]) <- NULL
   
   close(progress_bar)
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Save total list with p tables & effect sizes ####
-save(p_list, file=paste0(here::here(), "/results/p_1000_trails.RData"))
 save(d_list,  file=paste0(here::here(), "/results/d_1000_trails.RData"))
 
 
